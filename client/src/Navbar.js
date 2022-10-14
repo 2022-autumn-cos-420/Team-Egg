9629d657bb15ad910c61880182da070411413bd0
brennanpoitras <brennan.poitras@maine.edu>
2022-10-14 16:30:41 -0400

    Added text for login and signup

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,5 +1,9 @@
 export default function Navbar(){
     return <nav className = "nav"> 
         <a href = "/" className = "site-title"> BlackBear Square </a>
+        <ul>
+            <li><a href = "/login"> Log In</a></li>    
+            <li><a href = "/signup"> Sign Up</a></li>
+        </ul>
     </nav>
 }
