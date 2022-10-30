ab149365127effaec9bf633ddfc37cd8a8a97464
brennanpoitras <brennan.poitras@maine.edu>
2022-10-30 18:12:06 -0400

    Added a test.
    First test that checks BlackBear Square is in the document.

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders blackbear square text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/BlackBear Square/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test", () => { 
+    test('renders blackbear square text', () => {
+      render(<App />);
+      const linkElement = screen.getByText(/BlackBear Square/i);
+      expect(linkElement).toBeInTheDocument();
+    });
 });
