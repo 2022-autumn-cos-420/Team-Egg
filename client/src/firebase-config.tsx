acc9b2909f0c8bfff63992ea447811e988df3d55
TSchotter <troyswizard@gmail.com>
2022-11-04 14:43:29 -0400

    Create firebase-config.tsx

---
+++ client/src/firebase-config.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArSDXdVHodcBPsMFnkCr0TrGHIjjW_j1M",
+  authDomain: "bbsquare-4657f.firebaseapp.com",
+  projectId: "bbsquare-4657f",
+  storageBucket: "bbsquare-4657f.appspot.com",
+  messagingSenderId: "438412272822",
+  appId: "1:438412272822:web:72a02aefb1c71f52c0b217"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
