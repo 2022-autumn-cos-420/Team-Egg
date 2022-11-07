58bee1315558677d6dec9141093c4f36a24cceb1
Graham Bridges <gsbdd02@gmail.com>
2022-11-07 13:42:25 -0500

    made CreateReview component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import FindMatch from './components/Findmatch';
-import './styles.css'
+import './styles.css';
+import CreateReview from './pages/CreateReview';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <FindMatch></FindMatch>
+      <CreateReview></CreateReview>
     </div>
   );
 }
---
+++ client/src/pages/CreateReview.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { Button } from "react-bootstrap"
+
+export default function CreateReview(): JSX.Element {
+    
+    return(
+        <div>
+            Hello World
+        </div>
+    );
+}
+
