102080c07de17407f0d51bcb39e0cc32415e449e
TSchotter <troyswizard@gmail.com>
2022-12-06 18:28:40 -0500

    Created basic CourseDesc Box

--- client/src/pages/CreateCoursePage.tsx
+++ client/src/pages/CreateCoursePage.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
+function CourseDesc(
+    {courseDesc, setCourseDesc} :
+    {courseDesc: string,
+    setCourseDesc: (courseDesc: string) => void}
+
+): JSX.Element {
+    return <div data-testid="courseDesc" className="flex flex-col">
+        <label htmlFor="postText" className="">Course Description</label>
+         <textarea className="p-1 text-black border-2 border-black" id="postText" placeholder="Course Description Text"
+            onChange={(e)=>{setCourseDesc(e.target.value)}} value={courseDesc}></textarea>
+
+    </div>
+}
 
 
 
 
 function CreateCoursePage(): JSX.Element {
-    return <div data-testid="createCoursePage">Create Course Page</div>
+    const [courseDesc, setCourseDesc]  = useState<string>("");
+
+
+    return <div data-testid="createCoursePage">
+        <div>Create Course Page</div>
+        <CourseDesc courseDesc={courseDesc} setCourseDesc={setCourseDesc}/>
+    
+    </div>
 
 }
 
