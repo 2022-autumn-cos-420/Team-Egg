0a27ab6065c3fe6e4b0d022793010ab130527767
TSchotter <troyswizard@gmail.com>
2022-12-06 18:44:05 -0500

    added tests back in

--- client/src/pages/CreateCoursePage.test.tsx
+++ client/src/pages/CreateCoursePage.test.tsx
@@ -9,5 +9,33 @@ describe("CreateCoursePage Components Tests", () => {
         expect(x).toBeInTheDocument();
     });
 
-    
+    test("Course Title Component Renders", () => {
+        render(<CreateCoursePage />);
+        const x = screen.getByTestId("courseTitle");
+        expect(x).toBeInTheDocument();
+    });
+
+    test("Course Department Component Renders", () => {
+        render(<CreateCoursePage />);
+        const x = screen.getByTestId("courseDepartment");
+        expect(x).toBeInTheDocument();
+    });
+
+    test("Course Number Component Renders", () => {
+        render(<CreateCoursePage />);
+        const x = screen.getByTestId("courseNumber");
+        expect(x).toBeInTheDocument();
+    });
+
+    test("Course Describtion Component Renders", () => {
+        render(<CreateCoursePage />);
+        const x = screen.getByTestId("courseDesc");
+        expect(x).toBeInTheDocument();
+    });
+
+    test("Course CreditHours Component Renders", () => {
+        render(<CreateCoursePage />);
+        const x = screen.getByTestId("courseCreditHours");
+        expect(x).toBeInTheDocument();
+    });
 });
