8d194a707e860fd5a463804fb2fbd46d3a336b09
TSchotter <troyswizard@gmail.com>
2022-12-07 15:39:10 -0500

    added basic setCourseTitle component

--- client/src/pages/CreateCoursePage.tsx
+++ client/src/pages/CreateCoursePage.tsx
@@ -4,7 +4,6 @@ function CourseDesc(
     {courseDesc, setCourseDesc} :
     {courseDesc: string,
     setCourseDesc: (courseDesc: string) => void}
-
 ): JSX.Element {
     return <div data-testid="courseDesc" className="flex flex-col">
         <label htmlFor="postText" className="">Course Description</label>
@@ -14,16 +13,34 @@ function CourseDesc(
     </div>
 }
 
+function CourseTitle(
+    {courseTitle, setCourseTitle} :
+    {courseTitle: string,
+    setCourseTitle: (courseTitle: string) => void}
+): JSX.Element {
+    return <div data-testid="courseTitle" className="flex flex-col">
+        <label htmlFor="postText" className="">Course Description</label>
+         <textarea className="p-1 text-black border-2 border-black" id="postText" placeholder="Course Description Text"
+            onChange={(e)=>{setCourseTitle(e.target.value)}} value={courseTitle}></textarea>
+
+    </div>
+}
+
+
 
 
 
 function CreateCoursePage(): JSX.Element {
     const [courseDesc, setCourseDesc]  = useState<string>("");
-
+    const [courseTitle, setCourseTitle]  = useState<string>("");
+    const [courseCode, setCourseCode]  = useState<string>("");
+    const [courseNum, setCourseNum]  = useState<number>(0);
+    const [courseHours, setCourseHours] = useState<number>(0);
 
     return <div data-testid="createCoursePage">
         <div>Create Course Page</div>
-        <CourseDesc courseDesc={courseDesc} setCourseDesc={setCourseDesc}/>
+        <CourseTitle courseTitle={courseTitle} setCourseTitle={setCourseTitle} />
+        <CourseDesc courseDesc={courseDesc} setCourseDesc={setCourseDesc}></CourseDesc>
     
     </div>
 
