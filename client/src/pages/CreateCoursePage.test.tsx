4574aac79e446edbd350848205cab0a2650f88e0
TSchotter <troyswizard@gmail.com>
2022-12-08 15:11:28 -0500

    tests didn't like me using nav outside of router

--- client/src/pages/CreateCoursePage.test.tsx
+++ client/src/pages/CreateCoursePage.test.tsx
@@ -1,58 +1,62 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
 import CreateCoursePage from './CreateCoursePage';
+import { BrowserRouter } from 'react-router-dom';
 
 describe("CreateCoursePage Components Tests", () => {
+
+    const setup = () => render(<BrowserRouter><CreateCoursePage /></BrowserRouter>);;
+
     test("Main Containing Element renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("createCoursePage");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Title Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseTitle");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Department Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseDepartment");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Number Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseNumber");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Describtion Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseDesc");
         expect(x).toBeInTheDocument();
     });
 
     test("Course CreditHours Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseCreditHours");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Year Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseYear");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Semester Component Renders", () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseSemester");
         expect(x).toBeInTheDocument();
     });
 
     test("Course Page Submit button renders" , () => {
-        render(<CreateCoursePage />);
+        setup();
         const x = screen.getByTestId("courseSubmitButton");
         expect(x).toBeInTheDocument();
     });
