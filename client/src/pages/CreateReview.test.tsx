d5f500b428b74ec9d5c6c994294f64df2f2f3e46
Graham Bridges <gsbdd02@gmail.com>
2022-12-09 15:48:02 -0500

    update to createReview tests

--- client/src/pages/CreateReview.test.tsx
+++ client/src/pages/CreateReview.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
+import { render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CreateReview from './CreateReview';
 
-describe("CreateReview Components Tests", () => {
+describe("CreateReview Components Tests", () => {  
     test("Second page is hidden", () => {
         render(<CreateReview />);
         const hiddenText = screen.queryByText(/Back/);
