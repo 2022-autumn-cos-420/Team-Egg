4477f551f12513f4957e0f45bc8a871552ad5de7
TSchotter <troyswizard@gmail.com>
2022-12-10 10:59:45 -0500

    CourseSearchPage created (initial)

---
+++ client/src/pages/CourseSearchPage.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import CourseSearchPage from './CourseSearchPage';
+import { BrowserRouter } from 'react-router-dom';
+
+
+describe("CourseSearchPage Components Tests", () => {
+    
+
+});
---
+++ client/src/pages/CourseSearchPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function CourseSearchPage(): JSX.Element {
+    return <div></div>
+
+
+}
