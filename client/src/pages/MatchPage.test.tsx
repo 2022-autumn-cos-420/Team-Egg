7ae3bf9310c3e6dbcfea283a3984d7b16340088d
TSchotter <troyswizard@gmail.com>
2022-12-11 09:26:20 -0500

    fixed a few tests on the matchPagetest

--- client/src/pages/MatchPage.test.tsx
+++ client/src/pages/MatchPage.test.tsx
@@ -5,18 +5,18 @@ import MatchPage from './MatchPage'
 describe("Test", () => {
     test("Renders question 1", () => {
         render(<MatchPage/>);
-        const question1 = screen.getByText(/Select 3 majors you are interested in./i);
+        const question1 = screen.getByText(/Select the majors you are interested in./i);
         expect(question1).toBeInTheDocument();
     });
 
     test("Renders question 2 on next button click", () => {
         render(<MatchPage/>);
-        const question1 = screen.queryByText(/Select 3 majors you are interested in./i);
-        const question2 = screen.queryByText("How many hours are you willing to dedicate per week?");
+        const question1 = screen.queryByText(/Select the majors you are interested in./i);
         const nextButton = screen.getByRole("button", {name: "Next"});
 
         expect(question1).toBeInTheDocument();
         nextButton.click();
+        const question2 = screen.queryByText(/how many hours are you willing to deidcate to your courses per week?/i);
         expect(question2).toBeInTheDocument();
     });
 });
