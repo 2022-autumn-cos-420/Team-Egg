712978bed43e61d848b271f3b13bf875424f52d0
Graham Bridges <gsbdd02@gmail.com>
2022-12-16 18:02:01 -0500

    tests added to CreateReview

--- client/src/pages/CreateReview.test.tsx
+++ client/src/pages/CreateReview.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen} from "@testing-library/react";
+import { act, render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CreateReview from './CreateReview';
 
@@ -9,5 +9,27 @@ describe("CreateReview Components Tests", () => {
         const hiddenText = screen.queryByText(/Back/);
         expect(hiddenText).toBeNull();
     });
+    test("First page is hidden after Next button is clicked", () => {
+        render(<CreateReview />);
+        const nextButton = screen.getByTestId("ReviewNextButton1"); 
+        act(() => {
+            nextButton.click();
+          });
+        const hiddenText = screen.queryByText(/How did the class meet for lectures/i);
+        expect(hiddenText).toBeNull();
+    })
+    test("Render page three", () => {
+        render(<CreateReview />);
+        const nextButton = screen.getByTestId("ReviewNextButton1");
+        act(() => {
+            nextButton.click();
+          });
+        const nextButton2 = screen.getByTestId("ReviewNextButton2");
+        act(() => {
+            nextButton2.click();
+        })
+        const hiddenText = screen.queryByText(/Next/);
+        expect(hiddenText).toBeNull();
+    })
 
 });
